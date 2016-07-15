6c96b63d7dad4cadbd126cda3662463965ee0c6e
Aex12 <alexcastonmolero@gmail.com>
2016-07-16 00:30:29 +0200

    Simple example ping/pong

---
+++ example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from TinyPyBot import TinyPyBot
+
+token  = "xxxxxxxxxxxxxxxxxxxxxxxxx" #your bot token
+bot = TinyPyBot(token)
+
+while bot.getMessages():
+  if bot.message is None:
+		continue
+	
+	if "message" in bot.message:
+		message = bot.message["message"]
+		
+		if "text" in message:
+			print bot.whoSends(message) + ": " + message["text"]
+				
+		if "entities" in message:
+	    if "bot_command" == message["entities"][0]["type"] and 0 == message["entities"][0]["offset"]:
+			  if message["text"][1:].startswith("ping"):
+				  bot.sendMessage("pong", message["chat"]["id"])
+				
