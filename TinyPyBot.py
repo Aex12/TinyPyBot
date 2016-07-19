3a582e32d63e3986e44e144bc2d23fef72fb8f11
Aex12 <alexcastonmolero@gmail.com>
2016-07-19 13:24:34 +0200

    Added a message parser and newLine() method to inlineKeyboard class

--- TinyPyBot.py
+++ TinyPyBot.py
@@ -38,12 +38,6 @@ class TinyPyBot:
 		self.offset = self.message["update_id"]+1
 		return True
 
-	def whoSends(self, message):
-		firstName = message["from"]["first_name"] if "first_name" in message["from"] else ""
-		lastName  = " "+message["from"]["last_name"] if "last_name" in message["from"] else ""
-		userName  = " [@"+message["from"]["username"]+"]" if "username" in message["from"] else ""
-		return firstName+lastName+userName
-
 	def sendMessage(self, text, chat_id, params={}):
 		params.update({'chat_id': chat_id, 'text': text})
 		return self.request("sendMessage", params)
@@ -56,14 +50,22 @@ class TinyPyBot:
 		params.update({'chat_id': chat_id, 'message_id': message_id, 'text': text})
 		return self.request("editMessageText", params)
 
+
 class reply_markup:
 	class inlineKeyboard:
 		def __init__(self):
 			self.tojson = {"inline_keyboard": []}
+			self.line = []
 
 		def addButton(self, text, params={}):
 			params.update({"text": text})
-			self.tojson["inline_keyboard"].append([params, ])
+			self.line.append(params)
+			return self
+
+		def newLine(self):
+			self.tojson["inline_keyboard"].append(self.line)
+			self.line = []
+			return self
 
 		def getJson(self):
 			return json.dumps(self.tojson)
@@ -72,3 +74,110 @@ class reply_markup:
 		params.update({"force_reply": True})
 		if selective != 0: params.update({"selective": selective})
 		return json.dumps(params)
+
+
+class parse_message:
+	def __init__(self, message):
+		self.data = message
+
+	@property
+	def is_message(self):
+		if "message" in self.data:
+			return True
+		else:
+			return False
+
+	@property
+	def is_callback_query(self):
+		if "callback_query" in self.data:
+			return True
+		else:
+			return False
+
+	@property
+	def message_id(self):
+		if not self.is_message: return False
+		return self.data["message"]["message_id"]
+
+	@property
+	def text(self):
+		if not self.is_message: return False
+		return self.data["message"]["text"]
+
+	@property
+	def chat_id(self):
+		if not self.is_message: return False
+		return self.data["message"]["chat"]["id"]	
+	
+	@property
+	def has_entities(self):
+		if not self.is_message: return False
+		if "entities" in self.data["message"]:
+			return True
+		else:
+			return False
+
+	@property
+	def has_commands(self):
+		if not self.has_entities: return False
+		for entity in self.data["message"]["entities"]:
+			if entity["type"] == "bot_command": return True
+		return False
+
+	@property
+	def has_text(self):
+		if not self.is_message: return False
+		if "text" in self.data["message"]:
+			return True
+		else:
+			return False
+	
+
+	@property
+	def is_command(self):
+		if not self.has_entities: return False
+		if "bot_command" != self.data["message"]["entities"][0]["type"]: return False
+		if  0 == self.data["message"]["entities"][0]["offset"]:
+			return True
+		else:
+			return False
+
+	@property
+	def is_forwarded(self):
+		if not self.is_message: return False
+		if "forward_from_chat" in self.data["message"]:
+			return True
+		else:
+			return False
+	
+
+	@property
+	def command(self):
+		if not self.is_command: return False
+		return self.data["message"]["text"][1:]
+
+	@property
+	def from_id(self):
+		if self.is_message:
+			return self.data["message"]["from"]["id"]
+		elif self.is_callback_query:
+			return self.data["callback_query"]["from"]["id"]
+
+	@property
+	def whoSends(self):
+		if not self.is_message: return None
+		firstName = self.data["message"]["from"]["first_name"] if "first_name" in self.data["message"]["from"] else ""
+		lastName  = " "+self.data["message"]["from"]["last_name"] if "last_name" in self.data["message"]["from"] else ""
+		userName  = " [@"+self.data["message"]["from"]["username"]+"]" if "username" in self.data["message"]["from"] else ""
+		return firstName+lastName+userName
+
+	def forward_from_chat(self, key):
+		if not self.is_forwarded: return None
+		if key in self.data["message"]["forward_from_chat"]:
+			return self.data["message"]["forward_from_chat"][key]
+		else:
+			return None
+
+	def logIt(self):
+		if self.has_text:
+			print self.whoSends + ": " + self.text
