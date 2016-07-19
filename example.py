b1759b1f9b06cc447b3e47de7821ff792f0c1479
Aex12 <alexcastonmolero@gmail.com>
2016-07-19 13:36:31 +0200

    changed sendMessageReply() behaviour

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from TinyPyBot import TinyPyBot
+from TinyPyBot import TinyPyBot, parse_message
 
 
 token  = "xxxxxxxxxxxxxxxxxxxxxxxxx" #your bot token
@@ -10,19 +10,16 @@ bot = TinyPyBot(token)
 
 while bot.getMessages():
 	if bot.message is None: continue
-	
-	if "message" in bot.message:
-		message = bot.message["message"]
-		
-		if "text" in message:
-			print bot.whoSends(message) + ": " + message["text"]
-				
-		if "entities" in message:
-			if "bot_command" == message["entities"][0]["type"] and 0 == message["entities"][0]["offset"]:
-				if message["text"][1:].startswith("ping"):
-					bot.sendMessage("pong", message["chat"]["id"])
-				if message["text"][1:].startswith("pong"):
-					bot.sendMessage("ping", message["chat"]["id"])
-		if "text" in message:
-			if "hello" in message["text"]:
-				bot.sendMessageReply("Hi!", message)
+
+	message = parse_message(bot.message)
+	message.logIt()
+
+	if message.is_command:
+		if message.command.lower().startswith("ping"):
+			bot.sendMessage("pong", message["chat"]["id"])
+		if message.command.lower().startswith("pong"):
+			bot.sendMessage("ping", message["chat"]["id"])
+		continue
+
+	if "hello" in message.text:
+		bot.sendMessageReply("Hi!", message.data)
